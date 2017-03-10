8798fd17102d0b0d4875353919a79fb6453da205
iOSyoungBrother <zch@ZCHdeMac-mini.local>
2017-03-10 13:10:30 +0800

    release 1.0.0

---
+++ HHKJButton/HHKJButton.h
@@ -0,0 +1,13 @@
+//
+//  HHKJButton.h
+//  TestGithubBtn
+//
+//  Created by ZCH on 2017/3/10.
+//  Copyright © 2017年 HHKJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HHKJButton : UIButton
+
+@end
---
+++ HHKJButton/HHKJButton.m
@@ -0,0 +1,21 @@
+//
+//  HHKJButton.m
+//  TestGithubBtn
+//
+//  Created by ZCH on 2017/3/10.
+//  Copyright © 2017年 HHKJ. All rights reserved.
+//
+
+#import "HHKJButton.h"
+
+@implementation HHKJButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
